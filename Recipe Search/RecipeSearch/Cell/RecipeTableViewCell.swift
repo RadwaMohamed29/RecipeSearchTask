8e0f60733fa3e2d4956b068f788d6fb5ca46d0bc
RadwaMohamed29 <radwamohamed2979@gmail.com>
2022-08-24 22:02:59 -0700

    Recipes Search UI

---
+++ Recipe Search/RecipeSearch/Cell/RecipeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RecipeTableViewCell.swift
+//  Recipe Search
+//
+//  Created by Radwa on 24/08/2022.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var recipeSourceLbl: UILabel!
+    @IBOutlet weak var recipeTitleLbl: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeHealthLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
